da87978a80403ff668e460f0e152514af4820ff9
Farhad Ghayour <f@famo.us>
2015-07-12 22:36:33 -0700

    + copy clipboard

--- app/background.js
+++ app/background.js
@@ -3,7 +3,6 @@ function getCurrentTabUrl(cb) {
         active: true,
         currentWindow: true
     };
-
     chrome.tabs.query(queryInfo, function(tabs) {
         var url = tabs[0].url;
         console.assert(typeof url == 'string', 'tab.url should be a string');
@@ -14,10 +13,8 @@ function getCurrentTabUrl(cb) {
 function renderStatus(statusText, url) {
     document.getElementById('status').textContent = statusText;
     document.getElementById('url').value = url;
+    alert(statusText);
     copyToClipboard(url);
-
-    var position = marker.getPositionedURL();
-    alert(position);
 }
 
 function copyToClipboard(text) {
@@ -36,29 +33,26 @@ chrome.browserAction.setBadgeText({text: "Mark"});
 
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
     suggest([
-      {content: text + " one", description: "the first one"},
-      {content: text + " number two", description: "the second entry"}
+        {
+            content: text + " one",
+            description: "the first one"
+        },
+        {
+            content: text + " number two",
+            description: "the second entry"
+        }
     ]);
 });
+
 chrome.omnibox.onInputEntered.addListener(function(text) {
     alert('You just typed "' + text + '"');
 });
-
-// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-//     switch (request.type) {
-//         case "dom-loaded":
-//             alert('cool!');
-//         break;
-//     }
-// });
-//
-//
 
 document.addEventListener("DOMContentLoaded", function(event) {
     chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: 'clicked' }, function(response) {
-                alert(response.url);
+                renderStatus('COPIED!', response.url);
             });
         });
     });
